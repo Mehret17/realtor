6d51d8214fec4889f64860be4a914bcd31b3baca
Mehret Kulala <merutaye@gmail.com>
2018-07-02 19:28:28 -0500

    select a listing works

--- src/components/Building/Building.js
+++ src/components/Building/Building.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
-import { listingShape } from '../../propz/listingsProp';
+import { listingOptionalShape } from '../../propz/listingsProp';
 import { formatPrice } from '../../helper';
 import './Building.css';
 
 class Building extends React.Component {
-  static propTypes = { listing: listingShape };
-  render() {
+  static propTypes = { listing: listingOptionalShape };
+  render () {
     const { listing } = this.props;
-    if (!listing) {
+    if (listing.nope) {
       return (
-        <h1> oh No </h1>
+        <div className="Building text-center">
+          <h1 className="unknown-listing"><span className="glyphicon glyphicon-arrow-left"></span>Select a listing! </h1>
+        </div>
       );
     }
     return (
       <div className="Building">
         <div className="row">
           <div className="col-xs-6">
-            <img className="building-image" src={listing.imageUrl} alt="Selected Building"/>
+            <img className="building-image" src={listing.imageUrl} alt="Selected Building" />
           </div>
           <div className="col-xs-6">
             <h3>{formatPrice(listing.price)}</h3>
@@ -25,6 +27,7 @@ class Building extends React.Component {
             <h4>{listing.address} {listing.city}, {listing.state} {listing.zip}</h4>
             <h5>{listing.numBeds} Bed/{listing.numBaths} Batch</h5>
             <h5>{listing.squareFootage} ft<sup>2</sup></h5>
+            <p>{listing.description}</p>
             <p>{listing.description}</p>
           </div>
         </div>
